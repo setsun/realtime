7ed3842d24c8981ef4cd990e43f8d0a24b51ce5c
Setsun <setsun@setsun.io>
2020-04-01 01:30:12 -0400

    init

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export enum SignalingEvents {
+  SendOffer = "SendOffer",
+  ReceiveOffer = "ReceiveOffer",
+  SendAnswer = "SendAnswer",
+  ReceiveAnswer = "ReceiveAnswer",
+  SendICECandidate = "SendICECandidate",
+  ReceiveICECandidate = "ReceiveICECandidate",
+}
