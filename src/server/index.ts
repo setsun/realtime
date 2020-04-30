81d31fa748d1f20f042c0dff73df81d3905929ad
Setsun <setsun@setsun.io>
2020-04-29 20:46:14 -0400

    in-progress work of migrating off of socket.io

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,75 +1,119 @@
-import { Server as HTTPServer } from "http";
-import SocketIO, { Socket } from "socket.io";
+import { Server as HTTPServer, IncomingMessage } from "http";
+import WebSocket, { Server as WebSocketServer } from "ws";
 import { SignalingEvents } from "../constants";
 
-export function createSignalingServer(httpServer: HTTPServer): HTTPServer {
-  const activeSockets: Map<String, Socket> = new Map();
-  const socketServer = SocketIO(httpServer);
+type SignalingServerConfig = {
+  server: HTTPServer;
+}
 
-  socketServer.on("connection", (socket) => {
-    console.log("Connect", { socket: socket.id });
+type BroadcastArgs = {
+  currentSocket: WebSocket;
+  eventName: string;
+  data: Object;
+}
 
-    if (activeSockets.has(socket.id)) {
-      return;
-    }
+export class SignalingServer {
+  activeSockets: Map<String, WebSocket> = new Map();
+  socketServer: WebSocketServer;
 
-    // add user to current active sockets
-    activeSockets.set(socket.id, socket);
+  constructor({ server }: SignalingServerConfig) {
+    this.socketServer = new WebSocketServer({ server });
+    this.setupSockets();
+  }
 
-    const activeSocketIds = [...activeSockets.keys()];
+  getSocketKey(request: IncomingMessage) {
+    const keys = request.headers['sec-websocket-key'];
+    return Array.isArray(keys) ? keys.join('') : keys;
+  }
 
-    console.log({ activeSocketIds })
+  broadcast({ currentSocket, eventName, data }: BroadcastArgs) {
+    this.socketServer.clients.forEach((client) => {
+      if (client.readyState !== WebSocket.OPEN) return;
+      if (client === currentSocket) return;
 
-    // emit active users to current client
-    socket.emit(SignalingEvents.Initialize, {
-      me: socket.id,
-      users: activeSocketIds,
-    });
-
-    // emit new user connection to all other clients
-    socket.broadcast.emit(SignalingEvents.UserConnected, {
-      user: socket.id,
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Disconnect", { socket: socket.id });
-
-      activeSockets.delete(socket.id);
-
-      socket.broadcast.emit(SignalingEvents.UserDisconnected, {
-        user: socket.id
+      client.emit(eventName, () => {
+        return data;
       });
     });
+  }
 
-    // establish the intial RTC offer
-    socket.on(SignalingEvents.SendOffer, ({ to, offer }: any) => {
-      console.log(SignalingEvents.SendOffer, { to, offer });
+  setupSockets() {
+    const { socketServer, activeSockets } = this;
 
-      socket.to(to).emit(SignalingEvents.ReceiveOffer, {
-        offer,
-        from: socket.id,
+    socketServer.on("connection", (socket, request) => {
+      const socketKey = this.getSocketKey(request);
+
+      console.log("Connect", { socketKey });
+
+      if (activeSockets.has(socketKey)) {
+        return;
+      }
+
+      // add user to current active sockets
+      activeSockets.set(socketKey, socket);
+
+      const activeSocketIds = [...activeSockets.keys()];
+
+      console.log({ activeSocketIds })
+
+      // emit current socket key, and active users to current client
+      socket.emit(SignalingEvents.Initialize, {
+        me: socketKey,
+        users: activeSocketIds,
+      });
+
+      // broadcast new user connection to all other clients
+      this.broadcast({
+        currentSocket: socket,
+        eventName: SignalingEvents.UserConnected,
+        data: {
+          user: socketKey
+        }
+      });
+
+      socket.on("disconnect", () => {
+        console.log("Disconnect", { socketKey });
+
+        activeSockets.delete(socketKey);
+
+        // broadcast user disconnection to all other clients
+        this.broadcast({
+          currentSocket: socket,
+          eventName: SignalingEvents.UserDisconnected,
+          data: {
+            user: socketKey
+          }
+        });
+      });
+
+      // establish the intial RTC offer
+      socket.on(SignalingEvents.SendOffer, ({ to, offer }: any) => {
+        console.log(SignalingEvents.SendOffer, { to, offer });
+
+        activeSockets.get(to).emit(SignalingEvents.ReceiveOffer, {
+          offer,
+          from: socketKey,
+        });
+      });
+
+      // confirm the RTC answer and connect the two clients
+      socket.on(SignalingEvents.SendAnswer, ({ to, answer }: any) => {
+        console.log(SignalingEvents.SendAnswer, { to, answer });
+
+        activeSockets.get(to).emit(SignalingEvents.ReceiveAnswer, {
+          answer,
+          from: socketKey,
+        });
+      });
+
+      socket.on(SignalingEvents.SendCandidate, ({ to, candidate }: any) => {
+        console.log(SignalingEvents.SendCandidate, { to, candidate });
+
+        activeSockets.get(to).emit(SignalingEvents.ReceiveCandidate, {
+          candidate,
+          from: socketKey
+        });
       });
     });
-
-    // confirm the RTC answer and connect the two clients
-    socket.on(SignalingEvents.SendAnswer, ({ to, answer }: any) => {
-      console.log(SignalingEvents.SendAnswer, { to, answer });
-
-      socket.to(to).emit(SignalingEvents.ReceiveAnswer, {
-        answer,
-        from: socket.id,
-      });
-    });
-
-    socket.on(SignalingEvents.SendCandidate, ({ to, candidate }: any) => {
-      console.log(SignalingEvents.SendCandidate, { to, candidate });
-
-      socket.to(to).emit(SignalingEvents.ReceiveCandidate, {
-        candidate,
-        from: socket.id
-      });
-    });
-  });
-
-  return httpServer;
+  }
 }
